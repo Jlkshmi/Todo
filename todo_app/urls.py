9142932c99dbcd8a3beb3b3e7daafd2c322773d0
Jlkshmi <jayalakshmibinija2018@gmail.com>
2023-11-23 11:37:20 +0530

    second commit

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -3,8 +3,11 @@ from django.urls import path
 from todo_app import views
 
 urlpatterns = [
-    path('',views.test1,name='new1'),
-    path('page1',views.test2,name='new2'),
+    # path('',views.test1,name='new1'),
+    path('',views.test2,name='new2'),
     path('page2',views.test3,name='new3'),
-    path('page3',views.create,name='new4')
+    path('page3',views.create,name='page3'),
+    path('page4',views.read,name='page4'),
+    path('delt/<int:id>/',views.delt,name='delt'),
+    path('update/<int:id>/',views.update,name='update')
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from todo_app import form
 from todo_app.form import TodoForm
+from todo_app.models import Todo
 
 
 # Create your views here.
@@ -19,5 +20,29 @@ def create(request):
         data=TodoForm(request.POST)
         if data.is_valid():
             data.save()
-    return render(request,"indexdash1.html",{'form':form})
+            return redirect('page4')
+    return render(request,"create.html",{'form':form})
 
+def read(request):
+    data = Todo.objects.all()
+    return render(request,"read.html",{'data':data})
+
+#delete
+
+def delt(request,id):
+    if request.method =="POST":
+
+        data=Todo.objects.get(id=id)
+        data.delete()
+        return redirect('new4')
+
+def update(request,id):
+    todo=Todo.objects.get(id=id)
+    form= TodoForm(instance=todo)
+    if request.method == 'post':
+        form=TodoForm(request.POST,instance=todo)
+        if form.is_valid():
+            form.save()
+        return redirect('new5')
+
+    return render(request,'update.html',{'form':form})
